d927dd2408a2ff4748fc5e5f7d67e62ff2894d59
ashiyaman <ashiya1005@gmail.com>
2025-08-13 16:55:37 +0530

    fix css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,49 +1,51 @@
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
-    const user = useSelector(state => state.user)
+  const user = useSelector((state) => state.user);
 
-    return (
-        <nav className="navbar bg-neutral shadow-sm">          
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">DevTinder</a>
+  return (
+    <nav className="navbar bg-neutral shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">DevTinder</a>
+      </div>
+      {user && (
+        <div className="flex gap-2 mx-4 items-center">
+          <p>Welcome, {user.firstName}</p>
+          <div className="dropdown dropdown-end ">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
             </div>
-            <div className="flex gap-2 mx-4">
-                <div className="dropdown dropdown-end">
-                    {user && 
-                        <div
-                        tabIndex={0}
-                        role="button"
-                        className="btn btn-ghost btn-circle avatar"
-                        >
-                        <div className="w-10 rounded-full">
-                            <img
-                            alt="Tailwind CSS Navbar component"
-                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                            />
-                        </div>
-                        </div>}
-                    <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-                    >
-                    <li>
-                        <a className="justify-between">
-                        Profile
-                        <span className="badge">New</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a>Settings</a>
-                    </li>
-                    <li>
-                        <a>Logout</a>
-                    </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
-}
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
 
 export default Navbar;
