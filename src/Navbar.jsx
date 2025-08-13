ab3423612dcdf96afa813c2b40ace256e80533e3
ashiyaman <ashiya1005@gmail.com>
2025-08-13 16:36:32 +0530

    chore: Display profile pic

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 const Navbar = () => {
+    const user = useSelector(state => state.user)
+
     return (
         <nav className="navbar bg-neutral shadow-sm">          
             <div className="flex-1">
@@ -6,18 +10,19 @@ const Navbar = () => {
             </div>
             <div className="flex gap-2 mx-4">
                 <div className="dropdown dropdown-end">
-                    <div
-                    tabIndex={0}
-                    role="button"
-                    className="btn btn-ghost btn-circle avatar"
-                    >
-                    <div className="w-10 rounded-full">
-                        <img
-                        alt="Tailwind CSS Navbar component"
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                        />
-                    </div>
-                    </div>
+                    {user && 
+                        <div
+                        tabIndex={0}
+                        role="button"
+                        className="btn btn-ghost btn-circle avatar"
+                        >
+                        <div className="w-10 rounded-full">
+                            <img
+                            alt="Tailwind CSS Navbar component"
+                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                            />
+                        </div>
+                        </div>}
                     <ul
                     tabIndex={0}
                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
