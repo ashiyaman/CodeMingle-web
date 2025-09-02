6b9b1b528b11312ef4cee4a0e47c27a089122e7d
ashiyaman <ashiya1005@gmail.com>
2025-09-02 18:19:37 +0530

    fix: Fix navbar UI for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,67 +6,63 @@ import { removeUser } from "../utils/userSlice";
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleLogout = async() => {
-    try{
+  const handleLogout = async () => {
+    try {
       await axios.post(BASE_URL + "/logout", {
-        withCredentials: true
-      })
-      dispatch(removeUser())
-      return navigate("/login")
+        withCredentials: true,
+      });
+      dispatch(removeUser());
+      return navigate("/login");
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-      console.log(error)
-    }
-  }
-
+  };
   return (
-    <nav className="navbar bg-neutral shadow-sm">
-      <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
-      </div>
-      {user && (
-        <div className="flex gap-2 mx-4 items-center">
-          <p>Welcome, {user.firstName}</p>
-          <div className="dropdown dropdown-end ">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt={user.firstName}
-                  src={user.photoUrl}
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <Link to="/profile" className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/connections">Connections</Link>
-              </li>
-              <li>
-                <Link to="/requests">Requests</Link>
-              </li>
-              <li>
-                <Link onClick={handleLogout}>Logout</Link>
-              </li>
-            </ul>
-          </div>
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">CodeMingle</a>
         </div>
-      )}
-    </nav>
+        {user && (
+          <div className="flex items-center">
+            <p className="px-2 text-right ">Welcome, {user.firstName}</p>
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img alt={user.firstName} src={user.photoUrl} />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <Link to="/profile" className="justify-between">
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/connections">Connections</Link>
+                </li>
+                <li>
+                  <Link to="/requests">Requests</Link>
+                </li>
+                <li>
+                  <Link onClick={handleLogout}>Logout</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
